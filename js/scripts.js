4abb713c91a2ff5eb7145ac7c8a0d71720569e7b
leon <leonmonyoro@gmail.com>
2019-03-20 10:03:47 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2){
+return number1 + number2;
+};
+var number1 = parseInt(prompt("77"));
+var number2 = parseInt(prompt("88"));
+var result = add(number1, number2);
+alert(result);
